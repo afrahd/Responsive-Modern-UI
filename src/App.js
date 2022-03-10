a56eec2690ce12977aa2151a1b072f7fbf60aeba
Afra Dawood <3frahmalik@gmail.com>
2022-03-10 16:35:34 +0400

    Navbar update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
-function App() {
-  return <div className="App"></div>;
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
+import { CTA, Brand, Navbar } from './components'
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="gradient__bg">
+<Navbar />
+<Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+
+
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
