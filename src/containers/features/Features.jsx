830b3509a55c7487d949b637872902008a09e3af
Afra Dawood <3frahmalik@gmail.com>
2022-03-13 10:28:36 +0400

    Formatting

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import './features.css'
-import { About } from '../../components'
-
+import React from "react";
+import "./features.css";
+import { About } from "../../components";
 
 const Features = () => {
   return (
-    <div className='gpt3__features' id="features">
-      <div className='gpt3__features-header'>
-        <h1>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+    <div className="gpt3__features" id="features">
+      <div className="gpt3__features-header">
+        <h1>
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
         <p>Request Early Access to Get Started</p>
       </div>
-      <div className='gpt3__features-container'>
-      <About 
-      title="Improving end distrusts instantly"
-      text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
-      />
-      <About 
-      title="Become the tended active"
-      text="Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
-      />
-      <About 
-      title="Message or am nothing"
-      text="Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
-      />
-      <About 
-      title="Really boy law county"
-      text="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
-      />
+      <div className="gpt3__features-container">
+        <About
+          title="Improving end distrusts instantly"
+          text="From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+        />
+        <About
+          title="Become the tended active"
+          text="Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+        />
+        <About
+          title="Message or am nothing"
+          text="Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+        />
+        <About
+          title="Really boy law county"
+          text="Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
+        />
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
